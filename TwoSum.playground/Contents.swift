5543f5b51a3991cd75deabeb8ed78c221603660a
Kumar, Amit <kumaramit0226@gmail.com>
2020-08-05 11:10:08 +0530

    Update in Two sum calculation

--- TwoSum.playground/Contents.swift
+++ TwoSum.playground/Contents.swift
@@ -2,19 +2,15 @@ import UIKit
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var hashMap: [Int:Int] = [:]
-    var finalIndexes: [Int] = []
     for i in 0..<nums.count {
         let a = target - nums[i]
-        hashMap[a] = i
-    }
-    for j in 0..<nums.count {
-        if hashMap[nums[j]] != nil {
-            finalIndexes.append(j)
-            finalIndexes.append(hashMap[nums[j]] ?? -5)
-            break
+        guard let index = hashMap[nums[i]] else {
+            hashMap[a] = i
+            continue
         }
+        return [index,i]
     }
-    return finalIndexes
+    return []
 }
 
-print(twoSum([2,7,11,15], 9))
+print(twoSum([2,3,6,11,15], 9))
