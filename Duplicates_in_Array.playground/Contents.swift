ab17e5891beb7cba605d71b573e1c4aed01123ba
Kumar, Amit <kumaramit0226@gmail.com>
2020-08-06 14:01:30 +0530

    Find duplicates in an array

---
+++ Duplicates_in_Array.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+func findDuplicates(_ nums: [Int]) -> [Int] {
+    return Array(Dictionary(grouping: nums, by: { $0 }).filter { $1.count > 1 }.keys).sorted()
+}
+
+let duplicates = findDuplicates([4,3,2,7,8,2,3,1]) // [2,3]
