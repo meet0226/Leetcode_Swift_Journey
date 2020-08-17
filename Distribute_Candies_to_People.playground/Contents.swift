fbd3f61ce3f5aa15880ad2eedc69bfd5bbd36f0b
Kumar, Amit <kumaramit0226@gmail.com>
2020-08-17 18:37:33 +0530

    Distribute Candies to People

---
+++ Distribute_Candies_to_People.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+func distributeCandies(_ candies: Int, _ num_people: Int) -> [Int] {
+    var output = [Int](repeating: 0, count: num_people)
+    var remainingCandies = candies
+    var repeatCount = 0
+    for j in 1...candies {
+        if j > num_people {
+            remainingCandies -= j
+            if remainingCandies > 0 {
+                output[(j - 1) - (num_people * repeatCount)] += j
+            } else {
+                output[(j - 1) - (num_people * repeatCount)] += (remainingCandies + j)
+                return output
+            }
+        } else {
+            remainingCandies -= j
+            if remainingCandies > 0 {
+                output[j - 1] += j
+            } else {
+                output[j - 1] += (remainingCandies + j)
+                return output
+            }
+        }
+        repeatCount = j/num_people
+    }
+    return output
+}
+
+print(distributeCandies(60, 4)) // [15, 18, 15, 12]
