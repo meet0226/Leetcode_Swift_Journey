b10426e929ca92d60e45f4c1b147fd1ed169666b
Kumar, Amit <kumaramit0226@gmail.com>
2020-08-19 15:50:02 +0530

    Goat Latin

---
+++ Goat Latin.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+func toGoatLatin(_ S: String) -> String {
+    let stringArray = S.split(separator: " ")
+    var outputString = ""
+    let vowels = ["a","e","i","o","u"]
+    var suffix = "maa"
+    for var singleString in stringArray {
+        if let firstChar = singleString.first, !vowels.contains(String(firstChar.lowercased())) {
+            singleString.remove(at: singleString.startIndex)
+            singleString += String(firstChar)
+        }
+        singleString += suffix
+        suffix += "a"
+        outputString = String(outputString.isEmpty ? singleString : (outputString + (" " + singleString)))
+    }
+    return outputString
+}
+
+print(toGoatLatin("The quick brown fox jumped over the lazy dog"))
+
+// heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa
