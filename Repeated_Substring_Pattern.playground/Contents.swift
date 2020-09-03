f25144987dbcb2e9dece603a5c0290bc7bfa2651
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-03 17:15:26 +0530

    Repeated Substring Pattern

---
+++ Repeated_Substring_Pattern.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func repeatedSubstringPattern(_ s: String) -> Bool {
+    guard s.count > 1, (s.count % (s.count/2)) == 0 else { return false }
+    var ss = s + s
+    ss.removeLast()
+    ss.removeFirst()
+    return ss.contains(s)
+}
+
+print(repeatedSubstringPattern("abab"))
+print(repeatedSubstringPattern("aabb"))
+print(repeatedSubstringPattern("aba"))
+print(repeatedSubstringPattern("abc"))
+print(repeatedSubstringPattern("abca"))
+print(repeatedSubstringPattern("abcabcabcabc"))
+print(repeatedSubstringPattern("abcabdabcabdgabcabdabcabdg"))
+print(repeatedSubstringPattern("aaaaa"))
+print(repeatedSubstringPattern("ababba"))
+print(repeatedSubstringPattern("abac"))
+print(repeatedSubstringPattern("abacababacab"))
+
