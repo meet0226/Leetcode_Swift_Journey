d98ee5a5b5abc4f9b7aebcd6bf231fb0157e530e
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-07 13:55:57 +0530

    Word Pattern

---
+++ Word_Pattern.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func wordPattern(_ pattern: String, _ str: String) -> Bool {
+    let patternArray = Array(pattern)
+    let strArray = str.components(separatedBy: " ")
+    guard patternArray.count == strArray.count else { return false }
+    var strHashmap = [String: Character]()
+    var patternHashmap = [Character: String]()
+    for i in 0..<patternArray.count {
+        if strHashmap[strArray[i]] != nil || patternHashmap[patternArray[i]] != nil {
+            if patternArray[i] != strHashmap[strArray[i]] || strArray[i] != patternHashmap[patternArray[i]] {
+                return false
+            }
+        } else {
+            strHashmap[strArray[i]] = patternArray[i]
+            patternHashmap[patternArray[i]] = strArray[i]
+        }
+    }
+    return true
+}
+
+print(wordPattern("abba", "dog cat cat dog"))
+print(wordPattern("abba", "dog cat cat fish"))
+print(wordPattern("abba", "dog dog dog dog"))
+print(wordPattern("aaaa", "dog cat cat dog"))
