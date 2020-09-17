aea25653faebe3f2094d78f6ba9e6c47f5560459
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-17 11:16:08 +0530

    Maximum XOR of Two Numbers in an Array

---
+++ Maximum_XOR_of_Two_Numbers_in_an_Array.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+func findMaximumXOR(_ nums: [Int]) -> Int {
+    guard !nums.isEmpty else { return 0 }
+    var output = 0
+    for i in 0..<(nums.count-1) {
+        for j in (i+1)..<nums.count {
+            if (nums[i] ^ nums[j]) > output {
+                output = nums[i] ^ nums[j]
+            }
+        }
+    }
+    return output
+}
+
+print(findMaximumXOR([3,10,5,25,2,8]))
