065fa669e654d50b8120676cb245a2cc26ae7945
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-19 19:19:04 +0530

    Best Time to Buy and Sell Stock

---
+++ Best_Time_to_Buy_and_Sell_Stock.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func maxProfit(_ prices: [Int]) -> Int {
+    guard !prices.isEmpty else { return 0 }
+    var output = 0
+    var maxPrice = prices.last!
+    for index in stride(from: prices.count-2, through: 0, by: -1) {
+        if (maxPrice - prices[index]) > output {
+            output = maxPrice - prices[index]
+        }
+        maxPrice = (prices[index] > maxPrice) ? prices[index] : maxPrice
+    }
+    return output
+}
+
+print(maxProfit([7,1,5,3,6,4]))
+print(maxProfit([7,6,4,3,1]))
+print(maxProfit([3,2,6,5,0,3]))
+print(maxProfit([1,4,1,4,3,1]))
