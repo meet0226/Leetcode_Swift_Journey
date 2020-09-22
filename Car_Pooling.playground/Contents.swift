af9131aed90d305d60e7a3994755355d9dd0d580
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-22 09:47:48 +0530

    Car Pooling

---
+++ Car_Pooling.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+func carPooling(_ trips: [[Int]], _ capacity: Int) -> Bool {
+    let maxdistance = trips.reduce(0) { max($0, $1[2] ) }
+    var route = [Int](repeating: 0, count: maxdistance + 1)
+    for trip in trips {
+        route[trip[1]] += trip[0]
+        route[trip[2]] -= trip[0]
+    }
+    for i in 1...maxdistance {
+        route[i] += route[i - 1]
+        if route[i] > capacity {
+            return false
+        }
+    }
+    return true
+}
+
+print(carPooling([[2,1,5],[3,3,7]], 4))
+print(carPooling([[2,1,5],[3,5,7]], 4))
+print(carPooling([[3,2,7],[3,7,9],[8,3,9]], 11))
+
