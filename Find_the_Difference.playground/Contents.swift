9ee8c36766323dcd24d5f60a3c6100c9024ab93c
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-25 10:39:29 +0530

    Find the Difference

---
+++ Find_the_Difference.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func findTheDifference(_ s: String, _ t: String) -> Character {
+    var s = s
+    let arrayt = Array(t)
+    for char in arrayt {
+        if let index = s.firstIndex(of: char) {
+            s.remove(at: index)
+        } else {
+            return char
+        }
+    }
+    return Character("_")
+}
+
+func findTheDifference1(_ s: String, _ t: String) -> Character {
+    var res = s.unicodeScalars.reduce(0) { $0 ^ $1.value }
+    print(res)
+    res = t.unicodeScalars.reduce(res) { $0 ^ $1.value }
+    print(res)
+    return Character(UnicodeScalar(res)!)
+}
+
+print(findTheDifference("abcd","abcde"))
+print(findTheDifference1("abcd","abcda"))
