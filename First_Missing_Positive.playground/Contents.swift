533bb4525c161b90e07a867a11686fe9212a92f1
Kumar, Amit <kumaramit0226@gmail.com>
2020-09-30 18:11:38 +0530

    First Missing Positive

---
+++ First_Missing_Positive.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func firstMissingPositive(_ nums: [Int]) -> Int {
+    guard !nums.isEmpty else { return 1 }
+    for i in 1...nums.count {
+        if !nums.contains(i) {
+            return i
+        }
+    }
+    return nums.count+1
+}
+
+print(firstMissingPositive([3,4,-1,1]))
+print(firstMissingPositive([0,1,2,3]))
+print(firstMissingPositive([1,2,3,4]))
