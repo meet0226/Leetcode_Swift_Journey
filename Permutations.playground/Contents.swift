63068bead057bc1c296ee795b67860bd2e89ffe7
Kumar, Amit <kumaramit0226@gmail.com>
2020-10-03 16:04:51 +0530

    Permutations

---
+++ Permutations.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    guard !nums.isEmpty else { return [] }
+    var nums = nums
+    var output = [[Int]]()
+    findPermutations(&output, &nums, 0, nums.count - 1)
+    return output
+}
+
+func findPermutations(_ output: inout [[Int]],_ nums: inout [Int], _ start: Int, _ end: Int) {
+    if start == end {
+        output.append(nums)
+    } else {
+        for i in start...end {
+            nums.swapAt(start, i)
+            findPermutations(&output, &nums, start+1, end)
+            nums.swapAt(start, i)
+        }
+    }
+}
+
+print(permute([1,2,3]))
+
