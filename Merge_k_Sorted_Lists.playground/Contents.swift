16841404d2db44e40bb4143bc694ee8d74021fec
Kumar, Amit <kumaramit0226@gmail.com>
2020-10-16 18:27:51 +0530

    Merge k Sorted Lists

---
+++ Merge_k_Sorted_Lists.playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+func createList(data: [Int]) -> ListNode? {
+    guard !data.isEmpty else { return nil }
+    var rootNode: ListNode?
+    var currentNode: ListNode?
+    for value in data {
+        let newNode = ListNode(value)
+        if currentNode != nil {
+            currentNode?.next = newNode
+            currentNode = newNode
+        } else {
+            rootNode = newNode
+            currentNode = rootNode
+        }
+    }
+    return rootNode
+}
+
+func getNodeValue(root: ListNode?) {
+    if root != nil {
+        print(root!.val)
+        getNodeValue(root: root?.next)
+    }
+}
+
+func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+    guard !lists.isEmpty else { return nil }
+    var listNodes = [ListNode?]()
+    for list in lists {
+        var currentNode = list
+        while currentNode != nil {
+            listNodes.append(ListNode(currentNode!.val))
+            currentNode = currentNode?.next
+        }
+    }
+
+    listNodes.sort { $0!.val < $1!.val }
+    var outputNode: ListNode?
+    var currentNode: ListNode?
+    for node in listNodes {
+        if outputNode == nil {
+            outputNode = node
+            currentNode = outputNode
+        } else {
+            currentNode?.next = node
+            currentNode = node
+        }
+    }
+
+    return outputNode
+}
+
+let l1 = createList(data: [1,4,5])
+let l2 = createList(data: [1,3,4])
+let l3 = createList(data: [2,6])
+getNodeValue(root: mergeKLists([l1,l2,l3]))
+
+getNodeValue(root: mergeKLists([]))
+getNodeValue(root: mergeKLists([l1]))
