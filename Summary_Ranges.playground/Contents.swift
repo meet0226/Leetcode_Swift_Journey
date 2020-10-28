66d0945ec4dd32273d927177cf4b8718da306d72
Kumar, Amit <kumaramit0226@gmail.com>
2020-10-28 14:28:27 +0530

    Summary Ranges

---
+++ Summary_Ranges.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+func summaryRanges(_ nums: [Int]) -> [String] {
+    guard nums.count > 1 else {
+        return nums.isEmpty ? [] : [String(nums[0])]
+    }
+    var output = [String]()
+    var rangeStart = nums[0]
+    var rangeEnd = nums[0]
+    var index = 0
+    while index < nums.count {
+        if rangeEnd == nums[index] {
+            if index == nums.count - 1 {
+                if rangeStart == rangeEnd {
+                    output.append("\(rangeStart)")
+                } else {
+                    output.append("\(rangeStart)->\(rangeEnd)")
+                }
+            }
+            rangeEnd += 1
+            index += 1
+        } else {
+            if rangeStart == (rangeEnd - 1) {
+                output.append("\(rangeStart)")
+            } else {
+                output.append("\(rangeStart)->\(rangeEnd-1)")
+            }
+            rangeStart = nums[index]
+            rangeEnd  = nums[index]
+        }
+    }
+    return output
+}
+
+print(summaryRanges([0,1,2,4,5,7]))
+print(summaryRanges([0,1,2,4,5,6,7]))
+print(summaryRanges([0,1,2,3,4,5,6,7]))
