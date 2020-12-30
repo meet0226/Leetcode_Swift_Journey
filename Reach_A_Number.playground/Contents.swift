059e96ffdef4a9da59023d0334b18c14510fbf21
Kumar, Amit <kumaramit0226@gmail.com>
2020-12-30 09:49:27 +0530

    Reach A Number

---
+++ Reach_A_Number.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+func reachNumber(_ target: Int) -> Int {
+    let target = abs(target)
+    var step = 0
+    var sum = 0
+    while (sum < target) {
+        step += 1
+        sum += step
+    }
+    while ((sum - target) % 2 != 0) {
+        step += 1
+        sum += step
+    }
+    return step
+}
+
+print(reachNumber(5))
