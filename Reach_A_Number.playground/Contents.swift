c18de7e385399532c5eb703f15770d1e7b24a642
Kumar, Amit <kumaramit0226@gmail.com>
2020-12-30 10:18:23 +0530

    Reach A Number

--- Reach_A_Number.playground/Contents.swift
+++ Reach_A_Number.playground/Contents.swift
@@ -4,11 +4,7 @@ func reachNumber(_ target: Int) -> Int {
     let target = abs(target)
     var step = 0
     var sum = 0
-    while (sum < target) {
-        step += 1
-        sum += step
-    }
-    while ((sum - target) % 2 != 0) {
+    while (sum < target) || ((sum - target) % 2 != 0) {
         step += 1
         sum += step
     }
